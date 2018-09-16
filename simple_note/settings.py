9817c6350ce54d333ebaaab151c2ad5f8ede1c27
Yoon <yoon.s.hong@gmail.com>
2018-09-16 23:39:42 +0900

    Creating account application

--- simple_note/settings.py
+++ simple_note/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # Application definition
 
 INSTALLED_APPS = [
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
